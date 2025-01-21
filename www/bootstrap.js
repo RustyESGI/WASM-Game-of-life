79cbfb6da23466537f74d4e1f80808454440ac72
fhoxe <apedromonteiro@myges.fr>
2025-01-21 14:20:36 +0000

    [ADD] push www content

---
+++ www/bootstrap.js
@@ -0,0 +1,2 @@
+import("./index.js")
+  .catch(console.error);
---
+++ www/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ],
+  experiments: {
+    asyncWebAssembly: true
+  }
+};
